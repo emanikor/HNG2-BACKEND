08de56c6f43ed4126d5708599917fecbc72c089b
Emanikor <corneliusekai72@gmail.com>
2024-07-07 17:16:38 +0300

    Added new node_modules dependencies and validation utilities

--- config/config.js
+++ config/config.js
@@ -1,20 +1,11 @@
 const { Sequelize } = require('sequelize');
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env')});
-
-console.log('Database Config:', {
-  name: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  port: process.env.DB_PORT,
-});
+require('dotenv').config(); // Load environment variables
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
+  dialect: process.env.DB_DIALECT, // Ensure the dialect is set
   port: process.env.DB_PORT,
 });
 
 module.exports = sequelize;
+
